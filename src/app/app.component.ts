c5fced40a4d88312baee92d0e5282df0ca155728
daguttt <dagutmu667@gmail.com>
2025-03-05 18:47:42 -0500

    chore: format all project files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,7 @@ import { RouterOutlet } from '@angular/router';
   selector: 'dg-root',
   imports: [RouterOutlet],
   template: `
-    <h1>Welcome to {{title}}!</h1>
+    <h1>Welcome to {{ title }}!</h1>
 
     <router-outlet />
   `,
