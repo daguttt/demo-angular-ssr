563de54c7ae86c5275280e54367252250ca39108
daguttt <dagutmu667@gmail.com>
2025-03-17 22:01:52 -0500

    feat: conditionally choose request handler based on runtime provider

---
+++ src/netlify-request-handler.ts
@@ -0,0 +1,24 @@
+import { AngularAppEngine } from '@angular/ssr';
+import { getContext } from '@netlify/angular-runtime/context';
+
+function buildNetlifyAppEngineHandler(angularAppEngine: AngularAppEngine) {
+  return async function netlifyAppEngineHandler(
+    request: Request
+  ): Promise<Response> {
+    const context = getContext();
+
+    // Example API endpoints can be defined here.
+    // Uncomment and define endpoints as necessary.
+    // const pathname = new URL(request.url).pathname;
+    // if (pathname === '/api/hello') {
+    //   return Response.json({ message: 'Hello from the API' });
+    // }
+
+    const result = await angularAppEngine.handle(request, context);
+    return result || new Response('Not found', { status: 404 });
+  };
+}
+
+export function netlifyRequestHandler(angularAppEngine: AngularAppEngine) {
+  return buildNetlifyAppEngineHandler(angularAppEngine);
+}
